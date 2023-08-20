82d736fc8c13c1a0e1e29902bd82ed2f8072c118
Parth Thakral <parth.thakral72@gmail.com>
2023-08-20 14:20:06 +0530

    diff b/w strategy, adapter and factory

--- src/StrategyDP/after/GoogleMaps.java
+++ src/StrategyDP/after/GoogleMaps.java
@@ -7,5 +7,14 @@ public class GoogleMaps {
     public void findPath(String from, String to, Mode mode){
         PathCalculatorStrategy pathCalculatorStrategy = PathCalculatorStrategyFactory.getStrategy(mode);
         pathCalculatorStrategy.findPath(from, to);
+
+
+        // Here we have solved SRP, OCP
+        // Clints code is now looking much more simpler
+
+        // Adapter vs Strategy
+        // Adapter is structural, it is used when you are talking to 3rd party codebase(library)
+        // Strategy is behavioural, it is used when there are multiple ways to do something such that codebae is structured
+        // factory is creational, it used when we need to create cobject on a specific subclass
     }
 }
